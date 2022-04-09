7ee04287a9554ef8828ee507065601972654f2a3
Michael Moreno <michaelmoreno.dev@gmail.com>
2022-04-09 02:43:13 -0600

    Setup environment.

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import * as PIXI from 'pixi.js';
+
+const app = new PIXI.Application({
+    width: 800,
+    height: 600,
+    backgroundColor: 0x1099bb,
+    resolution: window.devicePixelRatio || 1,
+});
+
+document.body.appendChild(app.view);
+
+PIXI.utils.skipHello();
